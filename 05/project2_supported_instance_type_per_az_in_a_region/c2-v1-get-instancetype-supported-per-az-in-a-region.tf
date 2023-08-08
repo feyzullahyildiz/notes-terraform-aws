592f92d4ccb84a7e05d44c1b5c5deaecb0bd8233
A. Feyzullah YILDIZ <ahmet.feyzullah.yildiz@gmail.com>
2023-08-09 00:50:19 +0300

    aws_ec2_instance_type_offerings ile dinamik olarak instance aldık. Output'a nasıl basacağımızı tazeledik.

--- 05/project2_supported_instance_type_per_az_in_a_region/c2-v1-get-instancetype-supported-per-az-in-a-region.tf
+++ 05/project2_supported_instance_type_per_az_in_a_region/c2-v1-get-instancetype-supported-per-az-in-a-region.tf
@@ -1,18 +1,33 @@
 # https://registry.terraform.io/providers/hashicorp/aws/3.0.0/docs/data-sources/ec2_instance_type_offerings
 
-data "aws_ec2_instance_type_offerings" "my_ins_type1" {
+data "aws_ec2_instance_type_offerings" "my_ins_type2" {
+  for_each = toset(["us-east-1a", "us-east-1b", "us-east-1e"])
   filter {
     name   = "instance-type"
     values = ["t3.micro"]
   }
   filter {
     name   = "location"
-    values = ["us-east-1a"]
+    values = [each.key]
   }
   location_type = "availability-zone"
 
 }
 
-output "output_v1_1" {
-  value = data.aws_ec2_instance_type_offerings.my_ins_type1.instance_types
+output "output_v2_1" {
+  # this is not going to work for for_each
+  # value = data.aws_ec2_instance_type_offerings.my_ins_type1.instance_types
+
+  # value = [for t in data.aws_ec2_instance_type_offerings.my_ins_type2: toset(t.filter)]
+  value = [for t in data.aws_ec2_instance_type_offerings.my_ins_type2: t.instance_types]
+}
+# output "output_v2_2_feyz_method" {
+#   value = [for t in data.aws_ec2_instance_type_offerings.my_ins_type2: t.filter]
+# }
+
+output "output_v2_3" {
+  # for_each'deki her bir veri burada key olarak geliyor galiba
+  value = {
+    for key, value in data.aws_ec2_instance_type_offerings.my_ins_type2: key => value.instance_types
+  }
 }
